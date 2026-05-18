1576ad6785bdcdccc68258460db53367d5be3c49
Guru Venkat <mrguruvenkat@gmail.com>
2026-05-18 13:09:13 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,7 +0,0 @@
-const menuBtn = document.querySelector(".navbar-t");
-const navbar = document.querySelector(".navbar");
-
-menuBtn.addEventListener("click", () => {
-    menuBtn.classList.toggle("active");
-    navbar.classList.toggle("active");
-});
